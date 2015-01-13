4d1a3d3fd4c32512c7fed4cf42b43187e22fd43c
Davit Atam <asv492@live.com>
2015-01-13 12:40:30 +0100

    some actual code xml json serialization

---
+++ Exercise1/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise1
+{
+
+    [XmlInclude(typeof(Director))]
+    public class Director : Person
+    {
+
+    }
+    [XmlInclude(typeof(Manager))]
+    public class Manager : Person
+    {
+
+    }
+    [XmlInclude(typeof(TeamLeader))]
+    public class TeamLeader : Person
+    {
+    }
+    [XmlInclude(typeof(Engineer))]
+    public class Engineer : Person
+    {
+
+    }
+
+}
