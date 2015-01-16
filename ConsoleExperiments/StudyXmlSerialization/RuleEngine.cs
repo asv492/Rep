935aba3a9553928164882ab18d4e1569fd91595f
Davit Atam <asv492@live.com>
2015-01-16 16:25:42 +0100

    first rule and an unit test for it

---
+++ ConsoleExperiments/StudyXmlSerialization/RuleEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyXmlSerialization
+{
+    public class RuleEngine 
+    {
+        public Director DirectorObject { get; set; }
+
+        public static bool CheckRules(Director dir)
+        {
+            if (dir.IsEmployed && dir.Salary > 100)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ConsoleExperiments/UnitTestStudyXmlSerialization/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyXmlSerialization;
+
+
+namespace UnitTestStudyXmlSerialization
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AnyEmployedPersonMustHaveASalaryGreaterThan100()
+        {
+            var directorTest = new Director
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                ManagerId = 1,
+                IsEmployed = true,
+                Salary = 10,
+                Address = "Boschdijk",
+                DateOfBirth = new DateTime(1970, 12, 31),
+                SocialSecurityNo = 5235343
+            };
+
+            Assert.IsFalse(RuleEngine.CheckRules(directorTest));
+        }
+    }
+}
