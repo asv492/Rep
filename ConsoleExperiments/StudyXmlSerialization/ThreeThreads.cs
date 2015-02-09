cf85c232756d6fe09de1380b1469608da67747a8
Davit Atam <asv492@live.com>
2015-02-09 16:51:53 +0100

    threads and events

---
+++ ConsoleExperiments/StudyXmlSerialization/ThreeThreads.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudyXmlSerialization
+{
+    public class ThreeThreads
+    {
+        public static Queue<Person> PersonQueue = new Queue<Person>();
+        public static List<Person> RosterTest = new List<Person>();
+
+        public static void StartThreads()
+        {
+            //fill existing employees
+            FillRoster();
+
+            var roster = new Roster();
+            var rosterWatcher = new RosterWatcher(roster);
+
+            //3 threads
+            var addToQueueTask = new Task(() => FillQueue(1));
+            addToQueueTask.Start();
+            var watchQueue = new Task(() => QueueWatcher(2));
+            watchQueue.Start();
+
+        }
+
+        private static void QueueWatcher(int delayInSeconds)
+        {
+            while (true)
+            {
+                if (PersonQueue.Count != 0)
+                {
+                    var ros = new Roster();
+                    ros.AddPerson();
+
+                    Thread.Sleep(delayInSeconds * 1000);
+                }
+            }
+        }
+
+        private static void SerializeRoster()
+        {
+            var serializeTask = new Task(() =>
+                    {
+                        var typeOfExport = new XmlReadWrite();
+                        var employeeData = new GenericEmployeeData(typeOfExport);
+                        employeeData.WritePersonList(RosterTest);
+                    });
+            serializeTask.Start();
+        }
+        public class RosterWatcher
+        {
+            private Roster _roster;
+
+            public RosterWatcher(Roster roster)
+            {
+                _roster = roster;
+                _roster.RosterChanged += SerializeRoster;
+            }
+
+
+        }
+
+        public delegate void RosterChangedEventHandler();
+        public class Roster
+        {
+            public event RosterChangedEventHandler RosterChanged;
+            public void AddPerson()
+            {
+                var personToBeMoved = PersonQueue.Dequeue();
+                RosterTest.Add(personToBeMoved);
+                RosterChanged();
+            }
+        }
+
+
+
+        private static void FillRoster()
+        {
+            Company.EmployeeRoster.Clear();
+            Company.FillEmployeeRoster();
+            var personList = Company.EmployeeRoster;
+            foreach (var person in personList)
+            {
+                RosterTest.Add(person);
+            }
+        }
+
+        public static void FillQueue(int delayInSeconds)
+        {
+            Company.EmployeeRoster.Clear();
+            Company.FillEmployeeRoster();
+            var personList = Company.EmployeeRoster;
+            foreach (var person in personList)
+            {
+                PersonQueue.Enqueue(person);
+                Thread.Sleep(delayInSeconds * 1000);
+            }
+        }
+    }
+}
---
+++ ConsoleExperiments/UnitTestStudyXmlSerialization/ThreeThreadsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudyXmlSerialization
+{
+    [TestClass]
+    public class ThreeThreadsTest
+    {
+
+/*
+Thread1: add an employee to a brand new list (queue) one by one 
+        with a gap of 1sec (by reading from a file? or auto generating)
+               
+Thread2: remove an employee from that "brand new list", 
+        and insert it into "our list based db and rules"
+         
+Thread3: whenever a change is detected in our "list based db", serialize it
+*/
+
+
+
+        [TestMethod]
+        public void TestQueueIsFilledWithPersons()
+        {
+
+        ThreeThreads.StartThreads();
+            Console.WriteLine(ThreeThreads.PersonQueue);
+        }
+        [TestMethod]
+        public void TestQueueIsFilledWithOnePersonEachSecond()
+        {
+
+            ThreeThreads.StartThreads();
+            Console.WriteLine(ThreeThreads.PersonQueue);
+        }
+        [TestMethod]
+        public void ThreadsAreStarting()
+        {
+            ThreeThreads.StartThreads();
+        }
+    }
+
+    public class ThreadsExperiments
+    {
+        
+    }
+}
