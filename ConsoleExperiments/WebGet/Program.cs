fefd9e644afa5821ac503b6a17662cc5a59d468b
Davit Atam <asv492@live.com>
2015-04-19 12:52:56 +0200

    tried different logic

--- ConsoleExperiments/WebGet/Program.cs
+++ ConsoleExperiments/WebGet/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
-
 /*
  * Write a program which will:
 1) accept an url and a destination path as command line arguments
@@ -17,15 +18,17 @@ using System.Threading.Tasks;
 5) download Level N
  * 
  */
+using System.Windows.Forms;
 
 namespace WebGet
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             //TODO add N (= depth level) as parameter
-            int depthLevel = 0;
+            int depthLevel = 1;
             int currentDepthLevel = 0;
             // Test if input arguments were supplied: 
             if (args.Length != 2)
@@ -40,12 +43,12 @@ namespace WebGet
             string destinationUrl = args[0];
             string localStoragePath = args[1];
 
-
+            //TODO don't download the same page twice
 
             System.Console.WriteLine("Downloading page " + destinationUrl + " to " + localStoragePath + "...");
             //get the date and time to use for naming a new folder
             string timeStamp = DateTime.Now.ToShortDateString() + "_" + DateTime.Now.ToLongTimeString();
-            //replace : sign with _
+            //replace ":" sign with "_"
             string folderName = timeStamp.Replace(":", "-");
             //create subfolder with timestamp as name
             string subFolderPath = localStoragePath + "\\" + folderName;
@@ -53,7 +56,48 @@ namespace WebGet
             string wikiFolder = subFolderPath + "\\" + "wiki";
             System.IO.Directory.CreateDirectory(wikiFolder);
 
-            string pageContent = DownloadWebPage(destinationUrl, subFolderPath);
+            //different logic
+            //get page content
+            //for each link in page
+            /*
+             follow the link
+             * but how?
+             * create unique file name
+             * save page at the link with the unique file name
+             * replace the link in page content with the unique file name
+             * 
+             * limit number of requests per second
+             * 
+         
+             */
+            string pageContent = DownloadHtmlPage(destinationUrl);
+
+            for (int i = 0; i < depthLevel; i++)
+            {
+                
+            }
+
+            var w = new WebClient();
+            string str = w.DownloadString("http://www.play-hookey.com/");
+
+
+            foreach (LinkItem i in LinkFinder.Find(str))
+            {
+                Console.WriteLine(i);
+            }
+
+            //var wb = new WebBrowser { Url = new Uri(destinationUrl) };
+            //Thread.Sleep(3000);
+            //Console.WriteLine(wb.DocumentText);
+
+            //wb.DocumentCompleted += wb_DocumentCompleted;
+
+            //string offlinePageContent = ""; //ChangeLinksForOfflineUse(pageContent);
+
+            //end of different logic
+
+            //string pageContent = DownloadWebPage(destinationUrl, subFolderPath);
+
             var urlList = GetUrlsInPage(pageContent);
             foreach (string s in urlList)
             {
@@ -68,6 +112,36 @@ namespace WebGet
                 Console.WriteLine(s);
             }
             Console.ReadLine();
+        }
+
+        static void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            HtmlDocument source = ((WebBrowser)sender).Document;
+
+            ExtractLink(source);
+
+        }
+
+
+
+        private static void ExtractLink(HtmlDocument source)
+        {
+
+            HtmlElementCollection anchorList = source.GetElementsByTagName("a");
+
+            foreach (var item in anchorList)
+            {
+
+                Console.WriteLine(((HtmlElement)item).GetAttribute("href"));
+
+            }
+
+        }
+
+        private static string ChangeLinksForOfflineUse(string pageContent)
+        {
+            throw new NotImplementedException();
         }
 
         private static string DownloadWebPage(string destinationUrl, string subFolderPath)
@@ -115,6 +189,115 @@ namespace WebGet
             }
             return urlList;
         }
+
+        //different logic
+        //get page content
+        //for each link in page
+        /*
+         follow the link
+         * but how?
+         * create unique file name
+         * save page at the link with the unique file name
+         * replace the link in page content with the unique file name
+         * 
+         * limit number of requests per second
+         * 
+         
+         */
+
+        private static string DownloadHtmlPage(string url)
+        {
+            string pageContent = null;
+            try
+            {
+                // Open a connection
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+                // set timeout for 10 seconds (Optional)
+                httpWebRequest.Timeout = 10000;
+
+                // Request response:
+                System.Net.WebResponse webResponse = httpWebRequest.GetResponse();
+
+                // Open data stream:
+                System.IO.Stream webStream = webResponse.GetResponseStream();
+
+                // Create reader object:
+                if (webStream != null)
+                {
+                    var streamReader = new System.IO.StreamReader(webStream);
+
+                    // Read the entire stream content:
+                    pageContent = streamReader.ReadToEnd();
+
+                    // Cleanup
+                    streamReader.Close();
+                }
+                webStream.Close();
+                webResponse.Close();
+            }
+            catch (Exception exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}", exception.ToString());
+                return null;
+            }
+
+            return pageContent;
+        }
+
+        public struct LinkItem
+        {
+            public string Href;
+            public string Text;
+
+            public override string ToString()
+            {
+                return Href + "\n\t" + Text;
+            }
+        }
+
+        static class LinkFinder
+        {
+            public static List<LinkItem> Find(string file)
+            {
+                List<LinkItem> list = new List<LinkItem>();
+
+                // 1.
+                // Find all matches in file.
+                MatchCollection m1 = Regex.Matches(file, @"(<a.*?>.*?</a>)",
+                    RegexOptions.Singleline);
+
+                // 2.
+                // Loop over each match.
+                foreach (Match m in m1)
+                {
+                    string value = m.Groups[1].Value;
+                    LinkItem i = new LinkItem();
+
+                    // 3.
+                    // Get href attribute.
+                    Match m2 = Regex.Match(value, @"href=\""(.*?)\""",
+                    RegexOptions.Singleline);
+                    if (m2.Success)
+                    {
+                        i.Href = m2.Groups[1].Value;
+                    }
+
+                    // 4.
+                    // Remove inner tags from text.
+                    string t = Regex.Replace(value, @"\s*<.*?>\s*", "",
+                    RegexOptions.Singleline);
+                    i.Text = t;
+
+                    list.Add(i);
+                }
+                return list;
+            }
+        }
+
+
+
 
     }
 }
